7b74229440bf68099551567b13a011fc39d8541f
arshad1416 <arshad14@gmail.com>
2019-05-16 16:23:57 -0400

    Set up axios and setting up the main.js for the api.

--- client/src/Components/Main/main.js
+++ client/src/Components/Main/main.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
 import Hero from '../Hero/hero';
 import './main.scss';
-// import Axios from 'axios';
+import Axios from 'axios';
 
 class Main extends Component {
+
+    componentDidMount {
+        
+    }
 
     render() {
         return (
