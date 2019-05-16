6eed2dad7bcb4822de5fa58ec64949fe6d24dab1
arshad1416 <arshad14@gmail.com>
2019-05-16 18:12:32 -0400

    Creating an axios request to get information from the API.

--- client/src/Components/Main/main.js
+++ client/src/Components/Main/main.js
@@ -1,18 +1,29 @@
 import React, { Component } from 'react';
 import Hero from '../Hero/hero';
+import HeroTable from '../HeroTable/herotable';
 import './main.scss';
 import Axios from 'axios';
+import URL from 'https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2014-01-01&endtime=2014-01-02';
 
 class Main extends Component {
 
-    componentDidMount {
-        
+    state = {quakeData: {features:[]}}
+
+    componentDidMount() {
+        Axios
+            .get(`${URL}`)
+            .then(response => {
+                this.setState({
+                    quakeData: response.data.features
+            });
+        })
     }
 
     render() {
         return (
             <>
                 <Hero />
+                <HeroTable quakeData = {this.state.quakeData} />
             </>
         );
     }
